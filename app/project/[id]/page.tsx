dfc184a2de0a7bd94876c3d826efaba38f8833e9
Bhavin-307 <sp1757@srmist.edu.in>
2025-02-28 21:46:42 +0530

    Update page.tsx

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,8 +1,21 @@
 import { URL } from "@/api/cron/route";
 import ProjectPage from "./ProjectPage";
 
+interface ProjectType {
+  id: string;
+  title: string;
+  details: string;
+  tags: string[];
+  category: string;
+  author: string;
+  avatar: string;
+  createdAt: string;
+  sourceCode: string;
+  liveLink: string;
+  image: string;
+}
 
-async function getProject(id: string) {
+async function getProject(id: string): Promise<ProjectType | null>  {
   try {
     const response = await fetch(`${URL}/project/${id}`, { cache: "no-store" });
     if (!response.ok) {
@@ -23,7 +36,6 @@ const Project = async ({ params }: { params: { id: string } }) => {
     return <div>Project not found.</div>;
   }
 
-  
   return (
     <>
       <ProjectPage project={project} />
