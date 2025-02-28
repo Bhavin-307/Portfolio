efbb7691d2c167b6b3513fa4742a4f66add6b46a
Bhavin-307 <sp1757@srmist.edu.in>
2025-02-28 20:21:08 +0530

    Update page.tsx

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,53 +1,18 @@
 import { URL } from "@/api/cron/route";
 import ProjectPage from "./ProjectPage";
-import { Metadata } from "next";
 
 async function getProject(id: string) {
   try {
-    const response = await fetch(`${URL}/project/${id}`, { cache: "no-store" }); 
+    const response = await fetch(`${URL}/project/${id}`, { cache: "no-store" });
     if (!response.ok) {
       throw new Error("Failed to fetch data");
     }
     const data = await response.json();
-    return data.project; 
+    return data.project;
   } catch (error) {
     console.error("Error fetching project data:", error);
     return null;
   }
-}
-
-export async function generateMetadata({
-  params,
-}: {
-  params: { id: string };
-}): Promise<Metadata> {
-  const id = params.id;
-  const project = await getProject(id);
-
-  const title = project?.title || "Default Project Title";
-  const description = project?.details || "Default Project Description";
-  
-
-  return {
-    title,
-    description,
-    keywords: project?.tags || ["projects", "portfolio", "example"],
-    openGraph: {
-      title,
-      description,
-     
-      siteName: "Bhavin Sali",
-      type: "website",
-     
-    },
-    twitter: {
-      title,
-      description,
-     
-      card: "summary_large_image",
-      site: "@bhavin_sali307",
-    },
-  };
 }
 
 const Project = async ({ params }: { params: { id: string } }) => {
